bc2c7d92b321ad386cb1cae8c58dd394e794ac88
RosarioRu <mariarruval@gmail.com>
2023-01-18 11:16:18 -0800

    Add placeholder cart component.

---
+++ src/components/Cart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Cart() {
+
+  const cartStyles={
+    width: "80%",
+    float: "center",
+    margin: "auto",
+  }
+
+  return (
+    <div style={cartStyles}>
+      <p>Placeholder for cart component</p>
+    </div>
+  );
+}
+
+export default Cart;
+
