c8e80e02f861c5307c02feea92a28f21b8a68c3f
RosarioRu <mariarruval@gmail.com>
2023-01-19 12:25:57 -0800

    Refactor shared styling to global stylesheet, finish header/navbar styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import logoBookstoreImage from './../img/logoBookstore.jpg';
+import "./../../src/index.css";
 
 
 function Header() {
 
   const headerStyles = {
-    border: "1px solid black",
-    
-    // width: "80%",
-    // float: "center",
-    // margin: "auto",
+    marginLeft: "4%",
   }
+    
 
   return (
+
     <React.Fragment>
-      <Navbar className="navbar fixed-top navbar-light bg-light py-0" style={headerStyles}>
+      <Navbar className="navbar navbar-custom-header fixed-top py-0">
         <div class="logo">
-          
+            <img class="navbar-brand" src={logoBookstoreImage} width="50" alt="logo for bookstore, black and white pic of books" style={headerStyles}></img>
         </div>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            {/* <span class="navbar-text">Bookstore</span> */}
             <Link to="/" class="nav-item nav-link">Home</Link>
             <Link to="/cart" class="nav-item nav-link">Cart</Link>
             <Link to="/sign-in" class="nav-item nav-link" href="#">Account log in</Link>
           </div>
         </div>
-      </Navbar>
+      </Navbar>  
     </React.Fragment>
 );
 
