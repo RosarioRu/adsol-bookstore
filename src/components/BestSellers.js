c5645d39591a905464c6d8e45917b2a6411bd16d
RosarioRu <mariarruval@gmail.com>
2023-01-20 12:43:13 -0800

    Copy over code from bestseller branch

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -18,3 +18,58 @@ function BestSellers() {
 
 }
 export default BestSellers;
+
+///
+import React, { useState, useEffect } from 'react';
+
+
+function BestSellers() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [bestSellers, setBestSellers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+          if (!response.ok) {
+            throw new Error(`${response.status}: ${response.statusText}`);
+          } else {
+            return response.json()
+          }
+        })
+      .then((jsonifiedResponse) => {
+          setBestSellers(jsonifiedResponse.results)
+          setIsLoaded(true)
+        })
+      .catch((error) => {
+        setError(error.message)
+        setIsLoaded(true)
+      });
+  }, [])
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>...Best Sellers Loading...</h1>;
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Best Sellers</h1>
+        <ul>
+          {bestSellers.map((book, index) =>
+            <li key={index}>
+              <h3>{book.title}</h3>
+              {/* <p>{book.author}</p> */}
+            </li>
+          )}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+
+}
+
+
+export default BestSellers;
