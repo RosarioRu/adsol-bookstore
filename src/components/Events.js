6c30254824f262e739a14713819855369e57e9be
RosarioRu <mariarruval@gmail.com>
2023-01-22 23:29:27 -0800

    Add 'events' placeholder section with styling.

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,14 +3,21 @@ import React from "react";
 function Events() {
 
   const eventsStyles = {
-    border: "2px solid yellow"
+    // border: "2px solid yellow",
+    backgroundColor: "rgb(229, 234, 229)",
+    border: "1px solid #cdd0d2",
+    boxShadow: "5px -5px 5px 0px #cdd0d2",
+    width: "70%",
+    margin: "3%",
+    padding: "2%",
   }
 
     
   return (
     <React.Fragment>
       <div style={eventsStyles}>
-        <p>Events go here</p>
+        <p>Placeholder for events</p>
+        <p> January Readings:</p>
       </div>
     </React.Fragment>
   );
